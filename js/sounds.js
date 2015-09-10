0105ae08bff184b4c8e113854ec74d2436363e17
Marina Sovic <sovic.marina@gmail.com>
2015-09-10 16:03:51 -0700

    Replace wav for mpr for sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,11 +1,11 @@
 function Sounds(){		
-	this.explosionSound = SoundPool(10, "sounds/explosion.wav", 0.2);
-	this.blackholeSound = SoundPool(10, "sounds/blackhole.wav", 0.25);
-	this.bubbleSound = SoundPool(10, "sounds/bubble.wav", 1);
-	this.pointSound = SoundPool(10, "sounds/point.wav", 0.25);
-	this.shootSound = SoundPool(10, "sounds/shoot.wav", 0.1);
-	this.clockSound = SoundPool(10, "sounds/clock.wav", 0.2);
-	this.dyingSound = SoundPool(10, "sounds/dying.wav", 0.2);
-	this.bulletexplosionSound = SoundPool(10, "sounds/bulletexplosion.wav", 0.1);
-	this.introSound = SoundPool(10, "sounds/intro.wav", 0.1, true);
+	this.explosionSound = SoundPool(10, "sounds/explosion.mp3", 0.2);
+	this.blackholeSound = SoundPool(10, "sounds/blackhole.mp3", 0.25);
+	this.bubbleSound = SoundPool(10, "sounds/bubble.mp3", 1);
+	this.pointSound = SoundPool(10, "sounds/point.mp3", 0.25);
+	this.shootSound = SoundPool(10, "sounds/shoot.mp3", 0.1);
+	this.clockSound = SoundPool(10, "sounds/clock.mp3", 0.2);
+	this.dyingSound = SoundPool(10, "sounds/dying.mp3", 0.2);
+	this.bulletexplosionSound = SoundPool(10, "sounds/bulletexplosion.mp3", 0.1);
+	this.introSound = SoundPool(10, "sounds/intro.mp3", 0.1, true);
 }
